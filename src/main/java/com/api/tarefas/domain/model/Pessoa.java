deba5176bb6e27bc6c07bb28d94196c6a2914d4f
johnviana <johnvianaluz@hotmail.com>
2023-08-09 15:46:58 -0300

    tabela pessoa criada

---
+++ src/main/java/com/api/tarefas/domain/model/Pessoa.java
@@ -0,0 +1,29 @@
+package com.api.tarefas.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "pessoa")
+@Data
+public class Pessoa implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idPessoa;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String departamento;
+
+    @OneToMany(mappedBy = "pessoaAlocada")
+    private  List<Tarefa> tarefas;
+
+}
