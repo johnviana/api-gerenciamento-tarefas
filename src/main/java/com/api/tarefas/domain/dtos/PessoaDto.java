fe0f055f6208714cb17f787b18585080ec5a0582
johnviana <johnvianaluz@hotmail.com>
2023-08-09 19:27:54 -0300

    Refatorando class pessoaDTO colocando construtores

--- src/main/java/com/api/tarefas/domain/dtos/PessoaDto.java
+++ src/main/java/com/api/tarefas/domain/dtos/PessoaDto.java
@@ -1,19 +1,27 @@
 package com.api.tarefas.domain.dtos;
 
+import com.api.tarefas.domain.model.Pessoa;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.BeanUtils;
 
 import java.util.UUID;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class PessoaDto {
 
     private UUID idPessoa;
-
     private String nome;
-
     private String departamento;
-
     private TarefaDto tarefas;
+
+    public PessoaDto(){}
+
+    public PessoaDto(Pessoa entity){
+        BeanUtils.copyProperties(entity, this);
+    }
+
 }
