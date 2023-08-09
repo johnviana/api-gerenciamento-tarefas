0403cc7ed6ead634cc47f48d7299ee14492027b6
johnviana <johnvianaluz@hotmail.com>
2023-08-09 18:46:52 -0300

    Criado class Pesssoa service

---
+++ src/main/java/com/api/tarefas/domain/service/PessoaService.java
@@ -0,0 +1,14 @@
+package com.api.tarefas.domain.service;
+
+import com.api.tarefas.domain.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PessoaService {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+
+}
