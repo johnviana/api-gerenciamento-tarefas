db767df0d4c4bd89fa3c8a39cb979bad22148ee0
johnviana <johnvianaluz@hotmail.com>
2023-08-09 18:50:54 -0300

    Criando Setter e Getter

--- src/main/java/com/api/tarefas/domain/dtos/PessoaDto.java
+++ src/main/java/com/api/tarefas/domain/dtos/PessoaDto.java
@@ -1,7 +1,12 @@
 package com.api.tarefas.domain.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
+@Getter
+@Setter
 public class PessoaDto {
 
     private UUID idPessoa;
--- src/main/java/com/api/tarefas/domain/dtos/TarefaDto.java
+++ src/main/java/com/api/tarefas/domain/dtos/TarefaDto.java
@@ -1,8 +1,12 @@
 package com.api.tarefas.domain.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
-
+@Getter
+@Setter
 public class TarefaDto {
 
     private UUID idTarefa;
