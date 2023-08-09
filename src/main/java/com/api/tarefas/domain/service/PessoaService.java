c2a39e0e8d565168e65e26b56cf8aa94921e86d5
johnviana <johnvianaluz@hotmail.com>
2023-08-09 19:58:42 -0300

    Metodo findById criado e findAll

--- src/main/java/com/api/tarefas/domain/service/PessoaService.java
+++ src/main/java/com/api/tarefas/domain/service/PessoaService.java
@@ -1,10 +1,14 @@
 package com.api.tarefas.domain.service;
 
+import com.api.tarefas.domain.dtos.PessoaDto;
 import com.api.tarefas.domain.model.Pessoa;
 import com.api.tarefas.domain.repository.PessoaRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PessoaService {
@@ -12,7 +16,21 @@ public class PessoaService {
     @Autowired
     private PessoaRepository pessoaRepository;
 
-    @Transactional
+    @Transactional(readOnly = true)
+    public List<PessoaDto> findAll(){
+        List<Pessoa> result =  pessoaRepository.findAll();
+        return result.stream().map(PessoaDto::new).toList();
+//        return result.stream().map(x -> new PessoaDto(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public PessoaDto findById(UUID id){
+        Pessoa result = pessoaRepository.findById(id).get();
+        return new PessoaDto(result);
+
+    }
+
+    @Transactional(readOnly = true)
     public Pessoa salvar(Pessoa pessoa) {
         return pessoaRepository.save(pessoa);
     }
