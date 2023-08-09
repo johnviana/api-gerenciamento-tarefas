607fa4eccab679e0ebcd29b4a696973f86d99bcd
johnviana <johnvianaluz@hotmail.com>
2023-08-09 18:49:26 -0300

    Criado class Tarefa DTO

---
+++ src/main/java/com/api/tarefas/domain/dtos/TarefaDto.java
@@ -0,0 +1,21 @@
+package com.api.tarefas.domain.dtos;
+
+import java.util.UUID;
+
+public class TarefaDto {
+
+
+    private UUID idTarefa;
+
+    private String titulo;
+
+    private String descricao;
+
+    private String prazo;
+
+    private String duracao;
+
+    private boolean finalizado;
+
+    private PessoaDto pessoaAlocada;
+}
