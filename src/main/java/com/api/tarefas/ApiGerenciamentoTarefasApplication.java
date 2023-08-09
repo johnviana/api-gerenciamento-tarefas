1c722c647bdbcda0fdbbe6bc5feb44dd8633c425
johnviana <johnvianaluz@hotmail.com>
2023-08-09 11:46:14 -0300

    Projeto Criado

---
+++ src/main/java/com/api/tarefas/ApiGerenciamentoTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.api.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciamentoTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciamentoTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/tarefas/ApiGerenciamentoTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciamentoTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
