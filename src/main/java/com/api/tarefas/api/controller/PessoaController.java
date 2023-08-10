a4d84cc1af9fbb1b091cd25ebe544925a7a646a1
johnviana <johnvianaluz@hotmail.com>
2023-08-10 09:04:08 -0300

    refatorando o metodo salvar

--- src/main/java/com/api/tarefas/api/controller/PessoaController.java
+++ src/main/java/com/api/tarefas/api/controller/PessoaController.java
@@ -1,9 +1,36 @@
 package com.api.tarefas.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api.tarefas.domain.model.Pessoa;
+import com.api.tarefas.domain.service.PessoaService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/api/pessoa")
+@RequestMapping(value = "/pessoa")
 public class PessoaController {
+
+    @Autowired
+    private PessoaService pessoaService;
+
+//    @GetMapping
+//    public List<PessoaDto> findAll(){
+//        List<PessoaDto> result = pessoaService.findAll();
+//        return result;
+//    }
+//
+//    @GetMapping(value = "/{id}")
+//    public PessoaDto findById(@PathVariable Long id){
+//        return pessoaService.findById(id);
+//    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Pessoa salvarCozinhar(@RequestBody @Valid Pessoa pessoa ){
+        Pessoa pessoaSalva = pessoaService.salvar(pessoa);
+        return pessoaService.salvar(pessoaSalva);
+
+    }
+
 }
